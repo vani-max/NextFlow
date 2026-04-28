c247ec1771c945796709006416d77c55dc0b6501
vani-max <vaanisharrmaa121@gmail.com>
2026-04-28 14:52:53 +0530

    fix:: improve status route with better error handling

---
+++ .trigger/tmp/build-jXGbYI/Desktop/NextFlow/trigger.config.mjs
@@ -0,0 +1,24 @@
+import {
+  defineConfig
+} from "../../chunk-X4R7EAUX.mjs";
+import "../../chunk-WZGQJWAS.mjs";
+import {
+  init_esm
+} from "../../chunk-FUV6SSYK.mjs";
+
+// trigger.config.ts
+init_esm();
+var trigger_config_default = defineConfig({
+  project: "proj_komoaxrxxnvayypdgzcd",
+  runtime: "node",
+  logLevel: "log",
+  maxDuration: 300,
+  dirs: ["src/trigger"],
+  build: {}
+});
+var resolveEnvVars = void 0;
+export {
+  trigger_config_default as default,
+  resolveEnvVars
+};
+//# sourceMappingURL=trigger.config.mjs.map
